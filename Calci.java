c8b05ec97ee468e94e4e72f8da8f4097a394588a
Mucool95 <85050961+Mucool95@users.noreply.github.com>
2022-12-26 16:03:46 +0530

    Final Commit

---
+++ Calci.java
@@ -0,0 +1,14 @@
+public class Calci {
+  
+ 
+    public static void main(String[] args) {
+
+    double ang = Math.toRadians(90.5);
+    System.out.println(Math.sin(ang));
+    System.out.println(Math.cos(ang));
+       ;
+    }
+    
+}
+
+
